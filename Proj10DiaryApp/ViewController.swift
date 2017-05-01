0281f780ec5f51010d6184e14e71575de9bede68
vmartin3 <vernonmartin11@gmail.com>
2017-05-01 15:15:16 -0400

    Initial Commit

---
+++ Proj10DiaryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proj10DiaryApp
+//
+//  Created by Vernon G Martin on 5/1/17.
+//  Copyright © 2017 Vernon G Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
